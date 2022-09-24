e0da0682b71d5318e16bd08bf02f108ece6a90d9
luotianyiyi <109797076+luotianyiyi@users.noreply.github.com>
2022-09-24 15:44:40 +0800

    Update GitTest.java

--- src/main/java/com/xja/git/GitTest.java
+++ src/main/java/com/xja/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Master!");
         System.out.println("hot-fix test!");
         System.out.println("Push Test!");
+        System.out.println("Pull Test!");
     }
 }
