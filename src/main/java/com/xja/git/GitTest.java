af0fa17c4a9bdd4d49000d24445105515f580271
admin <admin@xja.com>
2022-09-24 15:40:02 +0800

    push commit

--- src/main/java/com/xja/git/GitTest.java
+++ src/main/java/com/xja/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("fdsfda4");
         System.out.println("Master!");
         System.out.println("hot-fix test!");
+        System.out.println("Push Test!");
     }
 }
