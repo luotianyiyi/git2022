17c9a10555da38e38c24bb839233c1611239d1de
admin <admin@xja.com>
2022-09-24 15:01:25 +0800

    first commit

---
+++ src/main/java/com/xja/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xja.git;
+
+/**
+ * @author :ASUA
+ * @date: 2022/9/24 14:59
+ * @desc:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git Test!");
+    }
+}
